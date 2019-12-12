e302996416cae818bcd047d59dbc65f44a7caeb7
varunsapp <varuns@apptomate.co>
2019-12-12 11:42:31 +0530

    routes done

--- src/components/Display/ViewDisplay/index.jsx
+++ src/components/Display/ViewDisplay/index.jsx
@@ -2,14 +2,9 @@ import React, { Component, Fragment } from 'react';
 import { Tabs, Row, Col, Spin, Icon, Avatar } from 'antd';
 import { getDisplayDetails } from '../../../Redux/_actions';
 import { connect } from 'react-redux';
-
-import GoogleMapReact from 'google-map-react';
-import CustomMapMarker from '../../Common/CustomMapMarker';
 import DisplaySlider from '../../Common/DisplaySlider';
 import MarkerMap from '../../Common/googleMap/MarkerMap';
-
-const GOOGLE_MAP_API_KEY =
-  'AIzaSyCSTfXlm6bBFV5-o5RawVMKvhOd4foWnw4' || process.env.GOOGLE_MAP_API_KEY;
+import RouteCard from '../../Common/RouteCard';
 
 const { TabPane } = Tabs;
 
@@ -26,7 +21,6 @@ class ViewDisplay extends Component {
     const DisplayDetails = this.props.DisplayDetails.data || '{}';
     const { loading } = this.props.DisplayDetails;
     let {
-      name,
       categoryName,
       type,
       isPrivate,
@@ -43,18 +37,11 @@ class ViewDisplay extends Component {
     } = DisplayDetails;
     let routesData = routes || '[]';
     let parsedRoutes = JSON.parse(routesData);
-    parsedRoutes = parsedRoutes[0] || {};
 
-    let defaultCenter = {};
     if (latitude && longitude) {
       latitude = parseFloat(latitude);
       longitude = parseFloat(longitude);
-      defaultCenter = {
-        lat: latitude,
-        lng: longitude
-      };
     }
-    const defaultZoom = 15;
 
     return (
       <Fragment>
@@ -117,55 +104,11 @@ class ViewDisplay extends Component {
               </Row>
             </TabPane>
             <TabPane tab='Maping Routes' key='2'>
-              <div className='route-card'>
-                <Row>
-                  <Col className='route-maintitle' span={24}>
-                    <h3 className='route-name-h3'>'s Travel Route</h3>
-                  </Col>
-                  <Col span={12}>
-                    <div>
-                      <h4 className='route-title'>Route names</h4>
-                      <h4 className='route-title'>
-                        Cathedral of Saint Paul, St. Paul, Minnesota
-                      </h4>
-                      <div className='item-center list-username mt-1 mb-1'>
-                        <Avatar icon='user' />
-                        <span>User Name</span>
-                      </div>
-                      <div>
-                        <h4>Routes :</h4>
-                        <div className='route-name'>
-                          <Icon type='swap' />
-                          Oklahoma City
-                        </div>
-
-                        <div className='route-name'>
-                          <Icon type='swap' />
-                          Lake Aluma
-                        </div>
-
-                        <div className='route-name'>
-                          <Icon type='swap' />
-                          Forest Park
-                        </div>
-
-                        <div className='route-name'>
-                          <Icon type='swap' />
-                          McLoud
-                        </div>
-                      </div>
-                    </div>
-                  </Col>
-                  <Col span={12}>
-                    <div className='route-card-map'>
-                      <img
-                        src='https://miro.medium.com/max/5334/1*qYUvh-EtES8dtgKiBRiLsA.png'
-                        alt='no data'
-                      />
-                    </div>
-                  </Col>
-                </Row>
-              </div>
+              {!parsedRoutes.length && <p>No routes found</p>}
+              {parsedRoutes.map((route, key) => {
+                console.log(route);
+                return <RouteCard key={key} data={route} />;
+              })}
             </TabPane>
           </Tabs>
         </Spin>
