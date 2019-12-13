c137ab278672d18a09b92bdf5365a9e572bb80ae
SekarGaneshAppDev <ganeswarana@apptomate.co>
2019-12-13 16:58:12 +0530

    minor design

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {
   ProtectedRoute
 } from "./Redux/_service/AuthenticationService";
 import { Spin, Icon } from "antd";
+import "./index.css";
+
 //Gateway Routes
 const Login = React.lazy(() => import("./components/Login"));
 const AdminLayout = React.lazy(() => import("./components/AdminLayout"));
