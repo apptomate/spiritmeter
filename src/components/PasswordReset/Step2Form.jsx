2132da4fa9269f87e1ad72dec008bc78cd9ad1da
varunsapp <varuns@apptomate.co>
2019-12-17 18:41:50 +0530

    minors

--- src/components/PasswordReset/Step2Form.jsx
+++ src/components/PasswordReset/Step2Form.jsx
@@ -1,6 +1,5 @@
 import React, { Fragment, Component } from "react";
-import { Form, Icon, Input, Col, Button, Alert } from "antd";
-import { Link } from "react-router-dom";
+import { Form, Icon, Input, Col, Alert } from "antd";
 
 class PasswordResetStep2 extends Component {
   render() {
